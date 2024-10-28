313126ba6478e3eb0f9f7f28b658e2f38a704cfe
anishvankadhara <anishv0821@gmail.com>
2024-10-28 09:06:23 -0700

    Added Part 1
    Added Part 1 to Akul

---
+++ partnerPractice.java
@@ -0,0 +1,32 @@
+public class partnerPractice{
+
+public static void main(String[] args)
+{
+
+partnerPractice player = new partnerPractice();
+player.fillArray(3);
+
+
+}
+
+public int[][] fillArray(int num)
+{
+	int[][] arr2D = new int[num][num];
+	for(int i = 0; i < num;i++)
+	{
+		for(int j = 0; j < num; j++)
+		{
+
+		arr2D[i][j] = (int)((Math.random()*10)+1);
+		System.out.println(i + ":" + j + " " + arr2D[i][j]);
+
+		}
+
+	}
+
+return arr2D;
+
+
+}
+
+}
